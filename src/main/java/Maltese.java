cf78f9ca6d996ed2729f28ef46c64808e88fcee9
goodguyryan <flashsstudioz@gmail.com>
2025-08-21 18:50:39 +0800

    Change all Duke to Maltese and Add bootup message

---
+++ src/main/java/Maltese.java
@@ -0,0 +1,12 @@
+public class Maltese {
+    public static void bootup() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm Maltese, your personal chatbot!");
+        System.out.println("What can I do for you");
+        System.out.println("____________________________________________________________");
+        System.out.println("NOOOOOOOOOOOOO. See you soon o.0!");
+    }
+    public static void main(String[] args) {
+        bootup();
+    }
+}
