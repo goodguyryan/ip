a06b844287f5d28fd3bd340f57023562d39c43d6
goodguyryan <flashsstudioz@gmail.com>
2025-08-27 11:12:03 +0800

    Maltese.java: Add echoLoop function

--- src/main/java/Maltese.java
+++ src/main/java/Maltese.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Maltese {
     public static void bootup() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello I'm Maltese, your personal chatbot!");
         System.out.println("What can I do for you");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void echoLoop() {
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
+        System.out.println("____________________________________________________________");
         System.out.println("NOOOOOOOOOOOOO. See you soon o.0!");
+        System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
         bootup();
+        echoLoop();
     }
 }
