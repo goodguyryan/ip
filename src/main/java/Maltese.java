5ebc9313079eadb2467b03e562c33ace1f9f3f66
goodguyryan <flashsstudioz@gmail.com>
2025-08-28 16:43:07 +0800

    Maltese.java: Add Add Task and List function

--- src/main/java/Maltese.java
+++ src/main/java/Maltese.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 
 public class Maltese {
+    static String[] tasks = new String[100];
+    static int tasksLength = 0;
+
     public static void bootup() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello I'm Maltese, your personal chatbot!");
         System.out.println("What can I do for you");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void addTask(String task) {
+        tasks[tasksLength] = task;
+        tasksLength++;
+    }
+
+    public static void printList() {
+        for (int i = 0; i < tasksLength; i++) {
+            System.out.println((i + 1) + "." + tasks[i]);
+        }
     }
 
     public static void echoLoop() {
@@ -17,9 +31,14 @@ public class Maltese {
             if (line.equals("bye")) {
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+            if (line.equals("list")) {
+                printList();
+            } else {
+                System.out.println("____________________________________________________________");
+                addTask(line);
+                System.out.println("added: " + line);
+                System.out.println("____________________________________________________________");
+            }
         }
         System.out.println("____________________________________________________________");
         System.out.println("NOOOOOOOOOOOOO. See you soon o.0!");
